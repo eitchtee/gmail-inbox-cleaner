f6ab73221f9a4b69abade2eb6ff7a02a4cb3c12b
eitchtee <flaips.2@gmail.com>
2019-10-05 02:12:12 -0300

    added argparse support and gmail_api credentials for ease of use. Updated README with instructions.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gmail-inbox-cleaner
 Automatically archive and mark as read the e-mails on your inbox
+
+
+## How to use
+1. Clone this repository and `cd` into it.
+1. Run `pip install -r requirements.txt` (virtualenv is recommended)
+1. Run `python cleaner.py`
+
+
+    usage: cleaner.py [-h] [--age AGE] [--starred]
+    
+    optional arguments:
+      -h, --help  show this help message and exit
+      --age AGE   Archive e-mails older than x days.
+      --starred   Keep starred e-mails.
--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,4 @@
+import argparse
 import os.path
 import pickle
 from datetime import datetime
@@ -14,9 +15,9 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 def get_credentials():
     work_dir = os.path.dirname(os.path.realpath(__file__))
     google_api_path = os.path.normpath(
-        '{}/credenciais/gmail_api.json'.format(work_dir))
+        '{}/gmail_api.json'.format(work_dir))
     credencials_path = os.path.normpath(
-        '{}/credenciais/gmail_api_auth.db'.format(work_dir))
+        '{}/gmail_api_auth.db'.format(work_dir))
 
     creds = None
     # The file gmail_api_auth.db stores the user's access and refresh tokens,
@@ -41,7 +42,10 @@ def get_credentials():
     return creds
 
 
-def main(remove_emails_older_than: int = 30):
+def cleaner(args):
+    remove_emails_older_than = args.age
+    remove_starred = args.starred
+
     service = build('gmail', 'v1', credentials=get_credentials())
 
     # Call the Gmail API
@@ -51,6 +55,7 @@ def main(remove_emails_older_than: int = 30):
                                               maxResults=500, ).execute()
     results_list.append(results)
 
+    # Queries for next results until there aren't any (results > 500)
     if 'nextPageToken' in results.keys():
         appending = True
         while appending:
@@ -82,7 +87,20 @@ def main(remove_emails_older_than: int = 30):
         msg_date = datetime.fromtimestamp(int(msg['internalDate']) / 1000)
         today = datetime.now()
         x_days_ago = today - timedelta(days=remove_emails_older_than)
-        if 'STARRED' not in msg['labelIds'] and msg_date < x_days_ago:
+
+        if remove_starred:
+            remove_starred_cond = 'STARRED' not in msg['labelIds']
+        else:
+            remove_starred_cond = True
+
+        remove_emails_older_than = abs(remove_emails_older_than)
+
+        if remove_emails_older_than > 0:
+            remove_emails_older_than_cond = msg_date < x_days_ago
+        else:
+            remove_emails_older_than_cond = True
+
+        if remove_starred_cond and remove_emails_older_than_cond:
             archive_run = service.users(). \
                 messages(). \
                 modify(userId='me',
@@ -94,6 +112,17 @@ def main(remove_emails_older_than: int = 30):
         # else:
         #     print('{} is marked as important or too young. Skipped.'.format(
         #     email_id))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--age', type=int, default=30,
+                        help='Archive e-mails older than x days.')
+    parser.add_argument('--starred', dest='starred', action='store_true',
+                        help="Keep starred e-mails.")
+    parser.set_defaults(starred=False)
+    args = parser.parse_args()
+    cleaner(args)
 
 
 if __name__ == '__main__':
