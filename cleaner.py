31fafb062186a09a04e4044b966e11a8640e54da
eitchtee <flaips.2@gmail.com>
2020-05-16 08:58:13 -0300

    Changes oauth2 flow from local_server to console

--- cleaner.py
+++ cleaner.py
@@ -34,7 +34,7 @@ def get_credentials():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 google_api_path, SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
         # Save the credentials for the next run
         with open(credencials_path, 'wb') as token:
             pickle.dump(creds, token)
